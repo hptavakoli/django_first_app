3ec0b149c224b74d6f7902594e835c38c05e5e94
Hossein Pourtavakoli <h.ptavakoli@gmail.com>
2020-04-01 21:21:45 +0430

    Resolve Model Issue, Migrate it

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -11,15 +11,15 @@ class Topic(models.Model):
 
 
 class Webpage(models.Model):
-    topic = models.ForeignKey(Topic)
-    name = models.CharField(maz_length=264, unique=True)
+    topic = models.ForeignKey('Topic', on_delete=models.PROTECT)
+    name = models.CharField(max_length=264, unique=True)
     url = models.URLField(unique=True)
 
     def __str__(self):
         return self.name
 
 class AccessRecord(models.Model):
-    name = models.ForeignKey(Webpage)
+    name = models.ForeignKey('Webpage', on_delete=models.PROTECT)
     date = models.DateField()
 
     def __str__(self):
